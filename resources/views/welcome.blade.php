31a618064d44f8482ba782a4fa94f46d5388df29
Arya Firmansyah <aryafirman2003@gmail.com>
2024-05-05 16:05:32 +0700

    mengubah h1 pada welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
 
 <body>
     <div class="container text-center my-5">
-        <h1 class="mb-4">Belajar Controller & View pada Laravel</h1>
+        <h1 class="mb-4">Kolaborasi menggunakan GIT</h1>
         {{-- Contoh cara mereferensikan gambar di dalam file blade dengan
         menggunakan pendekatan Vite --}}
         <img class="img-thumbnail" src="{{ Vite::asset('resources/images/laravel.png') }}" alt="image" width="250">
